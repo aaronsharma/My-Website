91075d2b12322d5dc5454cb6a0d08842f4677f7c
Aaron Sharma <aaronsharma98@gmail.com>
2021-02-26 00:29:54 +0000

    added google adsense

--- src/html.js
+++ src/html.js
@@ -29,6 +29,7 @@ export default function HTML({
     </html>
   );
 }
+<script data-ad-client="ca-pub-6301471865736187" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
 HTML.propTypes = {
   htmlAttributes: PropTypes.object,
